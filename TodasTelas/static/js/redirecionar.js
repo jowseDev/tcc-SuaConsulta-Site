f56db303fe46e8028ede48a1c6ed15ec837eb6dd
José Augusto <jose2006martins@gmail.com>
2025-06-05 20:40:53 -0300

    versao 2.3-SiteSuaConsulta

--- TodasTelas/static/js/redirecionar.js
+++
@@ -1,7 +0,0 @@
-function registrar() {
-    window.location.href = '/auth/cadastro';  // Redireciona para a tela de cadastro
-}
-
-function logar() {
-    window.location.href = '/auth/login';  // Redireciona para a tela de login
-}
